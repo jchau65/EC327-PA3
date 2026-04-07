63bb5092b653c1080f206a7a1688a1ca2d50021f
jchau65 <jchau65@bu.edu>
2026-04-06 23:58:43 -0400

    Transfer started View

--- View.h
+++ View.h
@@ -1,8 +1,13 @@
 #ifndef VIEW_H
 #define VIEW_H
 
-class View {
+const int view_maxsize = 20; // The maximum size of the grid array
 
+class View {
+    private:
+        int size; // The current size of the grid to be displayed
+        double scale; // The distance represented by each cell of the grid
+        Point2D origin;
 };
 
 #endif
